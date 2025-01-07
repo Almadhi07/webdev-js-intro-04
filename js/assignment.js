8c8f4faab19971ecc224e54ba695b417ee575a31
Almadhi07 <aaalma05@louisville.edu>
2025-01-07 14:03:55 -0500

    Modified js file to conduct the appropriate age restriction for vote and alchol

--- js/assignment.js
+++ js/assignment.js
@@ -3,14 +3,20 @@
 // Create your references to the html elements here
 const ageInputEl = document.getElementById("age-input")
 const submissionBtn = document.getElementById("submission-btn");
-
+const response1 = document.getElementById("response");
 // create a mutable variable called age and do not assign it a value.
 
+let age;
 
 function checkAgeAndRespond() {
     age = parseInt(ageInputEl.value);
-    
-    // Write your code below
+    if(isNaN(age)) {
+        response1.textContent = "Please enter a valid number.";
+    } else if (age < 18) {
+        response1.textContent = "You are not old enough to proceed.";
+    } else {
+        response1.textContent = "Welcome! You are old enough to proceed.";
+    }
 
 }
 
